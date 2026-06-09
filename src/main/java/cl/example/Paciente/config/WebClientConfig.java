60fbcb51227461b8d3d9c21eb1f93fe2c9b47482
Matías <ma.beas@duocuc.cl>
2026-06-08 23:29:54 -0400

    Lo mismo que prevision excepto la seguridad (por tiempo)

--- src/main/java/cl/example/Paciente/config/WebClientConfig.java
+++ src/main/java/cl/example/Paciente/config/WebClientConfig.java
@@ -1,15 +1,20 @@
 package cl.example.Paciente.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+
+    @Value("${webclient.prevision.url}")
+    private String previsionUrl;
+
     @Bean
     public WebClient webClient() {
         return WebClient.builder()
-                .baseUrl("http://localhost:8081")
+                .baseUrl(previsionUrl)
                 .build();
     }
 }
